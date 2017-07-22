612344f3080cc669a2e58e035731f7ddd03c02e5
黎旭辉 <Jason@hajimuasahihuideMacBook-Pro.local>
2017-07-22 08:52:54 +0800

    添加通知栏remote弹窗与信息收发

--- ui/src/main/java/com/meizu/smartvoice/ui/MainActivity.java
+++ ui/src/main/java/com/meizu/smartvoice/ui/MainActivity.java
@@ -3,8 +3,11 @@ package com.meizu.smartvoice.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.meizu.common.widget.CompleteToast;
@@ -19,9 +22,12 @@ import flyme.support.v7.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity{
 
     public static final String TAG = "SmartVoice";
+    @BindView(R.id.content)
+    LinearLayout mContent;
 
     @BindView(R.id.begin_simulate)
     Button mbeginSimulate;
+    public static long mLastSendTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity{
         Activity_R.setStatusBarDarkIcon(this, true);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
     }
 
     @OnClick(R.id.begin_simulate)
@@ -36,12 +44,35 @@ public class MainActivity extends AppCompatActivity{
         Log.d(TAG, "begin_simulate");
         CompleteToast.makeText(this, "开始展示", Toast.LENGTH_SHORT).show();
         startService(new Intent(this, SmartVoiceService.class));
+
+        mLastSendTime = System.currentTimeMillis();
+        View send = LayoutInflater.from(this).inflate(R.layout.send, null);
+        mContent.addView(send);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mLastSendTime > 0) {
+            mContent.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    SmartVoiceService.getInstance().showNotification("路人甲", "正在回复", 1);
+                }
+            }, 2000);
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLastSendTime = 0;
+    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
-
 }
--- ui/src/main/java/com/meizu/smartvoice/ui/SmartVoiceService.java
+++ ui/src/main/java/com/meizu/smartvoice/ui/SmartVoiceService.java
@@ -2,6 +2,7 @@ package com.meizu.smartvoice.ui;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -11,8 +12,11 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
+import android.widget.RemoteViews;
 
 import com.meizu.smartvoice.R;
 import com.meizu.smartvoice.tools.PlayerManager;
@@ -23,6 +27,11 @@ public class SmartVoiceService extends Service implements SensorEventListener, P
     public static final String TAG = "SmartVoice";
 
     private final static String PATH = "/sdcard/smartvoice/weixin_message.mp3";
+    private static final String BUTTON_INDEX = "btn";
+    private static final String BUTTON_PLAY = "play";
+    private static final String BUTTON_CANCEL = "cancel";
+    private static final String BUTTON_REPLY = "reply";
+    RemoteViews mRemoteViews;
 
     private SensorManager mSensorManager;
     private Sensor mProximitySensor;
@@ -31,12 +40,19 @@ public class SmartVoiceService extends Service implements SensorEventListener, P
 
     private boolean mActive;
 
+    private static SmartVoiceService sInstance = null;
+
     public void onCreate() {
 
         mPlayerManager = PlayerManager.getInstance(this);
         mScreenUtil = ScreenUtil.getInstance(this);
         initSensor();
 
+        sInstance = this;
+    }
+
+    public static SmartVoiceService getInstance() {
+        return sInstance;
     }
 
     private void initSensor() {
@@ -120,7 +136,31 @@ public class SmartVoiceService extends Service implements SensorEventListener, P
                 .setLargeIcon(drawable.getBitmap())
                 .setOngoing(false)
                 .setAutoCancel(false);
+        mRemoteViews = new RemoteViews(getPackageName(), R.layout.noti_remote);
+        mRemoteViews.setViewVisibility(R.id.reply, View.INVISIBLE);
+        mRemoteViews.setViewVisibility(R.id.cancel, View.INVISIBLE);
 
+        //实例化一个指向MusicService的intent
+        Intent intent = new Intent(this, SmartVoiceService.class);
+        intent.setAction("com.meizu.voice");
+
+        //设置play按钮的点击事件
+        intent.putExtra(BUTTON_INDEX, BUTTON_PLAY);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.play, pendingIntent);
+
+        //设置next按钮的点击事件
+//        intent.putExtra(BUTTON_INDEX, BUTTON_CANCEL);
+//        pendingIntent = PendingIntent.getService(this, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+//        mRemoteViews.setOnClickPendingIntent(R.id.cancel, pendingIntent);
+//
+//        //设置prev按钮的点击事件
+//        intent.putExtra(BUTTON_INDEX, BUTTON_REPLY);
+//        pendingIntent = PendingIntent.getService(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        mRemoteViews.setOnClickPendingIntent(R.id.reply, pendingIntent);
+
+
+        builder.setContent(mRemoteViews);
         Notification notification = builder.build();
         notification.flags |= 0x04000000;
         notification.extras.putInt("flyme.showHeadUpInOccluded", 1);
@@ -128,5 +168,10 @@ public class SmartVoiceService extends Service implements SensorEventListener, P
         NotificationManager nm =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         nm.notify(notificationId, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
     }
 }
